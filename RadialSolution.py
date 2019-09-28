758076db56cb63536c57a742c1b81a8c75eb91a6
Sarel Kohen <kohen.s@husky.neu.edu>
2019-09-28 15:04:22 -0400

    Added median solution, however getting SQLite error.

--- RadialSolution.py
+++ RadialSolution.py
@@ -20,44 +20,52 @@ df = pd.read_csv(csvfile, skipinitialspace=True)
 
 # Create new SQLite instance and connection
 conn = sqlite3.connect(":memory:")
+curs = conn.cursor()
 
 # Convert Pandas DataFrame to SQLite
 df.to_sql(table_name, conn, if_exists='replace', index=False)
 
 # SQL Query that gets all required columns
-radial_query = "SELECT DISTINCT (h.CountyName || ', ' || h.State) as county_state, nh.num_hospitals, " \
-               "nach.num_acute_care_hospitals, printf(\"%.2f\", " \
-               "round(nach.num_acute_care_hospitals * 100) / nh.num_hospitals) as pct_acute_care, " \
-               "round(aacr.avg_acute_care_rating, 2) as avg_acute_care_rating, " \
-               "macr.median_acute_care_rating " \
-               "FROM HGI h " \
-               "LEFT JOIN (SELECT CountyName, count(*) as num_hospitals FROM HGI " \
-                          "GROUP BY CountyName " \
-                          "HAVING num_hospitals > 0) nh " \
-               "ON h.CountyName = nh.CountyName " \
-               "LEFT JOIN (SELECT CountyName, count(*) as num_acute_care_hospitals " \
-                          "FROM HGI " \
-                          "WHERE HospitalType LIKE 'Acute%' " \
-                          "GROUP BY CountyName) as nach " \
-               "ON nh.CountyName = nach.CountyName " \
-               "LEFT JOIN (SELECT CountyName, avg(Hospitaloverallrating) as avg_acute_care_rating " \
-                          "FROM HGI " \
-                          "WHERE HospitalType LIKE 'Acute%' AND Hospitaloverallrating NOT LIKE '%Not%' " \
-                          "GROUP BY CountyName) as aacr " \
-               "ON nach.CountyName = aacr.CountyName " \
-               "LEFT JOIN (SELECT CountyName, avg(Hospitaloverallrating) as median_acute_care_rating " \
-                          "FROM (SELECT CountyName, Hospitaloverallrating " \
-                                "FROM HGI " \
-                                "ORDER BY Hospitaloverallrating " \
-                                "LIMIT 2 - (SELECT COUNT(*) FROM HGI) % 2 " \
-                                "OFFSET (SELECT (COUNT(*) - 1) / 2 FROM HGI))) as macr " \
-               "ON aacr.CountyName = macr.CountyName " \
-               "ORDER BY num_hospitals DESC"
+radial_query = """SELECT DISTINCT nh.county_state, nh.num_hospitals, nach.num_acute_care_hospitals, 
+    printf("%.2f", round(nach.num_acute_care_hospitals * 100) / nh.num_hospitals) as pct_acute_care, 
+    round(aacr.avg_acute_care_rating, 2) as avg_acute_care_rating,
+    macr.median_acute_care_rating
+    FROM (SELECT (CountyName || ', ' || State) as county_state, count(*) as num_hospitals
+          FROM HGI
+          GROUP BY county_state
+          HAVING num_hospitals > 0) nh
+    JOIN (SELECT (CountyName || ', ' || State) as county_state, count(*) as num_acute_care_hospitals
+          FROM HGI
+          WHERE HospitalType LIKE 'Acute%'
+          GROUP BY county_state) as nach
+    ON nh.county_state = nach.county_state
+    JOIN (SELECT (CountyName || ', ' || State) as county_state, avg(Hospitaloverallrating) as avg_acute_care_rating
+          FROM HGI
+          WHERE HospitalType LIKE 'Acute%' AND Hospitaloverallrating NOT LIKE '%Not%'
+          GROUP BY county_state) as aacr
+    ON nach.county_state = aacr.county_state
+    JOIN (SELECT (CountyName || ', ' || State) as county_state , AVG(Hospitaloverallrating) as median_acute_care_rating
+          FROM (SELECT CountyName, State, Hospitaloverallrating,
+                ROW_NUMBER() OVER (
+                    PARTITION BY CountyName
+                    ORDER BY Hospitaloverallrating ASC, ProviderID ASC) AS RowAsc,
+                ROW_NUMBER() OVER (
+                    PARTITION BY CountyName
+                    ORDER BY Hospitaloverallrating DESC, ProviderID DESC) AS RowDesc
+                FROM HGI) x
+          WHERE RowAsc IN (RowDesc, RowDesc - 1, RowDesc + 1)
+          GROUP BY CountyName
+          ORDER BY CountyName) as macr
+    ON nh.county_state = macr.county_state
+    ORDER BY nh.num_hospitals DESC"""
 
 # Execute SQL Query
-results = pd.read_sql_query(radial_query, conn)
+#results = pd.read_sql_query(radial_query, conn)
+
+conn.execute(radial_query)
+print(curs.fetchall())
 
 conn.close()
 
 # Write the results of the SQL query onto a CSV file
-results.to_csv(path_to_export + '/hospitals_by_county.csv', index = None, header=True)
+# results.to_csv(path_to_export + '/hospitals_by_county.csv', index = None, header=True)
